a43ad1b86f1a0ae4bc72b34ad7d430f6252d4fbd
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-02-13 02:30:10 -0300

    chore: update config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://jagcruz.github.io",
-  base: "/",
+  site: 'https://jagcruz.github.io',
+  base: '/'
 });
