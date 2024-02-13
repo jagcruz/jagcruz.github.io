bc483d72495746b795483769e64ed3408e744f22
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-02-13 01:59:11 -0300

    feat: add Navbar and Hero components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: { extend: {} },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("tailwindcss-animated"),
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: { float: 'float 3s' },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-1.5rem)' },
+          '100%': { transform: 'translateY(0)' }
+        }
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animated')]
 };
