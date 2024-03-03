980ccc0d0deb7f61b3d5d7b755025b8e5e56690b
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-03-03 05:31:15 -0300

    feat: finish sidebar at Contact page

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,3 +1,19 @@
 export type THeaderItemId = 'home' | 'projects' | 'about' | 'contact';
 
 export type THeaderItem = { id: THeaderItemId; text: string; href: string };
+
+export const LINKS = {
+  GitHub: 'https://github.com/jagcruz',
+  LinkedIn: 'https://www.linkedin.com/in/jagcruz',
+  Facebook: 'https://www.facebook.com/jagutierrezcruz',
+  Discord: 'https://discordapp.com/users/719730313802154115',
+  Telegram: 'https://t.me/Tyto369',
+  Instagram: 'https://www.instagram.com/tyto369',
+  StackOverflow: 'https://stackoverflow.com/users/12452827/jagc',
+  X: 'https://twitter.com/Tyto993',
+  MyLocation: 'https://maps.app.goo.gl/PHw6yGWzK7A35w4L7'
+};
+
+export const WEBSITE_TITLE = 'JAGC | Software Developer';
+export const WEBSITE_DESCRIPTION =
+  "I'm Jaime Gutiérrez, Software Developer with more than 5 years of experience in creating digital solutions. My goal is to transform ideas into functional code and deliver products that make a difference for both my clients and their users.";
